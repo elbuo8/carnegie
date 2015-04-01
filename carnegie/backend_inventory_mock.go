86ea0216e84661c1b92dcc1d6319f22c42f0c75a
Yamil Asusta <yamil@rebelmail.com>
2015-04-01 17:16:38 -0400

    Perform actual lookups

--- carnegie/backend_inventory_mock.go
+++ carnegie/backend_inventory_mock.go
@@ -4,14 +4,18 @@ import (
 	"net/url"
 )
 
-type BackendInventoryMock struct{}
+type BackendInventoryMock struct {
+	Hosts map[string][]string
+}
 
 func (mock *BackendInventoryMock) GetBackends(host string) ([]*url.URL, error) {
 	var parsedURL *url.URL
 	var urls []*url.URL
-	if host == "google.com" {
-		parsedURL, _ = url.Parse("http://google.com")
-		urls = append(urls, parsedURL)
+	if backends, ok := mock.Hosts[host]; ok {
+		for i := 0; i < len(backends); i++ {
+			parsedURL, _ = url.Parse(backends[i])
+			urls = append(urls, parsedURL)
+		}
 	}
 	return urls, nil
 }
