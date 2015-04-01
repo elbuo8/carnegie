ea79ab8970b0e6a1282b113e347e694f3fe27891
William Kennedy <bill@ardanstudios.com>
2015-04-01 14:25:05 -0400

    Update carnegie.go
    Had time to look at this one file.

--- carnegie/carnegie.go
+++ carnegie/carnegie.go
@@ -1,3 +1,5 @@
+// BILL: Run through golint. Exported identifiers should have comments. It is hard
+//       to understand the entire story.
 package carnegie
 
 import (
@@ -19,20 +21,40 @@ func New(config *viper.Viper) (*Carnegie, error) {
 		return nil, err
 	}
 	config.SetDefault("interval", 60*time.Second)
-	carnegie := &Carnegie{
+	// BILL: Move this up
+	config.SetDefault("address", ":8181")
+	
+	// BILL: Don't create carnegie as a pointer.
+	//carnegie := &Carnegie{
+	//	Cache:         cache,
+	//	CacheInterval: config.GetDuration("interval"),
+	//}
+	carnegie := Carnegie{
 		Cache:         cache,
 		CacheInterval: config.GetDuration("interval"),
+		
+		// BILL: Why not include this in the composite literal?
+		Server:         &http.Server{
+			Addr:    config.GetString("address"),
+			Handler: http.HandlerFunc(carnegie.Handler),
+		}
 	}
-	config.SetDefault("address", ":8181")
-	srv := &http.Server{
-		Addr:    config.GetString("address"),
-		Handler: http.HandlerFunc(carnegie.Handler),
-	}
-	srv.SetKeepAlivesEnabled(false)
-	carnegie.Server = srv
-	return carnegie, nil
+	
+	carnegie.Server.SetKeepAlivesEnabled(false)
+
+	// BILL: Why not set this pointer from the beginning above?
+	//srv := &http.Server{
+	//	Addr:    config.GetString("address"),
+	//	Handler: http.HandlerFunc(carnegie.Handler),
+	//}
+	//srv.SetKeepAlivesEnabled(false)
+	//carnegie.Server = srv
+
+	//return carnegie, nil
+	return &carnegie, nil
 }
 
+// BILL: What happens if I call Start 1000 times.
 func (c *Carnegie) Start() error {
 	go c.UpdateCacheLoop()
 	return c.Server.ListenAndServe()
@@ -43,20 +65,32 @@ func (c *Carnegie) RoundTrip(r *http.Request) (*http.Response, error) {
 	if err != nil {
 		return nil, err
 	}
-	statusCode := res.StatusCode
-	if statusCode >= 500 {
+	
+	// BILL: Why the assignment to test the status code?
+	//statusCode := res.StatusCode
+	//if statusCode >= 500 {
+	//	c.Cache.Invalidate(r.Host)
+	//}
+	
+	if res.StatusCode >= 500 {
 		c.Cache.Invalidate(r.Host)
 	}
+	
 	return res, nil
 }
 
 func (c *Carnegie) UpdateCacheLoop() {
 	ticker := time.NewTicker(c.CacheInterval)
 	for {
-		select {
-		case <-ticker.C:
-			c.Cache.LocalInventory.Purge()
-		}
+		// BILL: No need for a select here since you are
+		// working with a single channel
+		<-ticker.C
+		c.Cache.LocalInventory.Purge()
+		
+		//select {
+		//case <-ticker.C:
+		//	c.Cache.LocalInventory.Purge()
+		//}
 	}
 }
 
@@ -67,6 +101,11 @@ func (c *Carnegie) Handler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	
+	// BILL: You are not checking if the urls slice is empty?
+	
+	// BILL: I don't know enough but creating this value for every request
+	//       scares me unless there is no other way.
 	proxy := httputil.NewSingleHostReverseProxy(urls[0])
 	proxy.Transport = c
 	proxy.ServeHTTP(w, r)
