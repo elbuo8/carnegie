8a0a4acce7199061d84f6cf98899d3edd7f9a726
William Kennedy <bill@ardanstudios.com>
2015-04-01 16:55:34 -0400

    Update consul_backend.go
    I might have a syntax error since coding in GitHub.  Changing the type to use an embedded field may cause syntax issue elsewhere.
    
    Not sure I love this one file existing for this one type. But I don't know enough of the story yet.

--- carnegie/consul_backend.go
+++ carnegie/consul_backend.go
@@ -7,18 +7,20 @@ import (
 	"strconv"
 )
 
+// BILL: Why not embed the pointer since this is the only
+//       type associated with this struct.
 type ConsulBackend struct {
-	Client *consulApi.Client
+	*consulApi.Client
 }
 
 func NewConsulBackend(config *viper.Viper) (*ConsulBackend, error) {
-	consulConfig := &consulApi.Config{
+	consulConfig := consulApi.Config{
 		Address: config.GetString("address"),
 		Scheme:  config.GetString("scheme"),
 		Token:   config.GetString("token"),
 	}
 
-	consulClient, err := consulApi.NewClient(consulConfig)
+	consulClient, err := consulApi.NewClient(&consulConfig)
 	if err != nil {
 		return nil, err
 	}
@@ -29,24 +31,34 @@ func NewConsulBackend(config *viper.Viper) (*ConsulBackend, error) {
 }
 
 func (cb *ConsulBackend) GetBackends(host string) ([]*url.URL, error) {
-	health := cb.Client.Health()
-	serviceEntries, _, err := health.Service(host, "", true, nil)
+	serviceEntries, _, err := cb.Health().Service(host, "", true, nil)
 	if err != nil {
 		return nil, err
 	}
-	totalEntries := len(serviceEntries)
-	urls := make([]*url.URL, totalEntries)
-	for i := 0; i < totalEntries; i++ {
-		address := "http://"
-		address += serviceEntries[i].Node.Address
-		if port := serviceEntries[i].Service.Port; port != 0 {
-			address += ":" + strconv.Itoa(port)
+	
+	// BILL: Why not use a for/range on serviceEntries?
+	urls := make([]*url.URL, len(serviceEntries))
+	for i, se := range serviceEntries {
+		address := fmt.Sprintf("http://%s", se.Node.Address)
+		if se.Service.Port != 0 {
+			address += ":" + strconv.Itoa(se.Service.Port)
 		}
-		parsedURL, err := url.Parse(address)
-		if err != nil {
+		if urls[i], err = url.Parse(address); err != nil {
 			return nil, err
 		}
-		urls[i] = parsedURL
 	}
+
+	//totalEntries := len(serviceEntries)
+	//urls := make([]*url.URL, totalEntries)
+	//for i := 0; i < totalEntries; i++ {
+	//	address := fmt.Sprintf("http://%s", serviceEntries[i].Node.Address)
+	//	if port := serviceEntries[i].Service.Port; port != 0 {
+	//		address += ":" + strconv.Itoa(port)
+	//	}
+	//	if urls[i], err = url.Parse(address); err != nil {
+	//		return nil, err
+	//	}
+	//}
+	
 	return urls, nil
 }
